7fda946c35b1585cad8c631a9fd6bd09c8789a2d
Ilja Dotšar <torvinster@gmail.com>
2016-03-31 00:27:00 +0300

    Revised brick script

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class Brick : MonoBehaviour
 {
 
-
+   
     void OnCollisionEnter2D(Collision2D col)
     {
+        
         if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
 
         float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
@@ -16,13 +17,16 @@ public class Brick : MonoBehaviour
         //decrease health according to magnitude of the object that hit us
         Health -= damage;
         //if health is 0, destroy the block
-        if (Health <= 0) 
-                Instantiate(remains, transform.position, transform.rotation);
-                Destroy(gameObject);
-        //This will create a copy of the remains at the exact position of the brick. Afterwards, the object will be removed, giving the illusion that the new one is actually the old one, but "broken".
-
+        if (Health <= 0)
+        {
+            
+            Destroy(gameObject);
+            Instantiate(Remains, transform.position, transform.rotation);
+            //This will create a copy of the remains at the exact position of the brick. Afterwards, the object will be removed, giving the illusion that the new one is actually the old one, but "broken".
+        }
+    
     }
-
+    public GameObject Remains;
     public float Health = 70f;
 
 
