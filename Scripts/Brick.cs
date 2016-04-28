e40868f8e22eb5ed7b1e4e8bef43c54f77ff1ed1
ikaine <indrek.kaine@gmail.com>
2016-04-28 07:37:45 +0300

    Script files change
    Current sounds that we have in the game are now affected by the settings, applied on main menu

---
+++ Scripts/Brick.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour
+{
+
+   
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+        //don't play audio for small damages
+        if (damage >= 10){
+			
+			//Play sound when it has been turned on
+			if(MainCode.Sound == 1){
+				GetComponent<AudioSource>().Play();
+			}
+		}
+        //decrease health according to magnitude of the object that hit us
+        Health -= damage;
+        //if health is 0, destroy the block
+        if (Health <= 0)
+        {
+            
+            Destroy(gameObject);
+           Instantiate(Remains, transform.position, transform.rotation);
+            //This will create a copy of the remains at the exact position of the brick. Afterwards, the object will be removed, giving the illusion that the new one is actually the old one, but "broken".
+        }
+    
+    }
+    public GameObject Remains;
+    public float Health = 70f;
+
+
+    //wood sound found in 
+    //https://www.freesound.org/people/Srehpog/sounds/31623/
+}
---
+++ Scripts/Enums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    //enums for the state of the slingshot, the 
+    //state of the game and the state of the bird
+    public enum CannonState
+    {
+        Idle,
+        UserAiming,
+        WolfFlying
+    }
+
+    public enum GameState
+    {
+        Start,
+        WolfMovingToCannon,
+        Playing,
+        Won,
+        Lost
+    }
+
+
+    public enum WolfState
+    {
+        BeforeShot,
+        Shot
+    }
+    
+}
